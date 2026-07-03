814b1c46d87735109f507a3ed0e165780434ea65
DivyankaPradhan <dp8321282@gmail.com>
2026-07-03 22:03:29 +0530

    Added solution - LeetHub

---
+++ 7882-integers-come-in-all-sizes-Hackerrank/7882-integers-come-in-all-sizes.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+g=a**b
+n=c**d
+print(f"{g+n}")
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
