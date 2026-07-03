16600badc104a72b95f5129ff0ae57034f91b79b
DivyankaPradhan <dp8321282@gmail.com>
2026-07-03 21:52:24 +0530

    Added solution - LeetHub

---
+++ 7883-arithmetic-operators-Hackerrank/7883-arithmetic-operators.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(f"{a+b}")
+    print(f"{a-b}")
+    print(f"{a*b}")
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
