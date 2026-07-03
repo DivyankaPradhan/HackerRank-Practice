96c6ec3ee9223a7d93fd7e89c1eb15d42c0dac72
DivyankaPradhan <dp8321282@gmail.com>
2026-07-03 21:53:10 +0530

    Added solution - LeetHub

---
+++ 7941-loops-Hackerrank/7941-loops.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0,n):   
+        print(f"{i*i}")
+    print()
+    
+    
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
