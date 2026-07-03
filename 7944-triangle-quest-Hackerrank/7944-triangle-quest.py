58b603e63e1e31aa100be8a4e242c416bf4c9c72
DivyankaPradhan <dp8321282@gmail.com>
2026-07-03 22:03:57 +0530

    Added solution - LeetHub

---
+++ 7944-triangle-quest-Hackerrank/7944-triangle-quest.py
@@ -0,0 +1,7 @@
+for i in range(1,int(input())):    
+    print(i*(10**i-1)//9)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
