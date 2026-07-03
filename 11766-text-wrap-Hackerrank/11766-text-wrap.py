8d14a3c4ef256d121016f308a332ed6ca0c24b8f
DivyankaPradhan <dp8321282@gmail.com>
2026-07-03 21:59:26 +0530

    Added solution - LeetHub

---
+++ 11766-text-wrap-Hackerrank/11766-text-wrap.py
@@ -0,0 +1,10 @@
+
+def wrap(string, max_width):
+    h=textwrap.fill(string,max_width)    
+    return h
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
