fb4f08ed60f3b5fa5d203e8c44afee5b56276234
DivyankaPradhan <dp8321282@gmail.com>
2026-07-18 11:33:59 +0530

    Force committed solution - Time: O(n * m), Space: O(n * m)

---
+++ 12524-matrix-script-Hackerrank/12524-matrix-script.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+
+m = int(first_multiple_input[1])
+
+matrix = []
+
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+raw=""
+for col in range(m):
+    for row in range(n):
+        raw+=matrix[row][col]
+dec=re.sub(r'(?<=\w)[^\w]+(?=\w)',' ',raw)
+print(dec)        
+    
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
